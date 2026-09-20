0e0b54f47dd98dc04aecbf3492345dc89c54b03f
Kosi-Kyrian <kosikyrian1@gmail.com>
2026-09-20 18:25:19 +0100

    hashed password, created and moved required codes into config and secret files

---
+++ config.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+import secret
+
+engine= create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
+SessionLocal= sessionmaker(autocommit= False, autoflush= False, bind= engine)
