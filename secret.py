f781d7c032dbb1a820059fbc356f92a16d637027
Kosi-Kyrian <kosikyrian1@gmail.com>
2026-09-24 22:29:39 +0100

    completed login endpoint, verified password and generated token

--- secret.py
+++ secret.py
@@ -7,3 +7,5 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL")
 OTP_EXPIRATION=int(os.getenv("OTP_EXPIRATION"))
+JWT_SECRET= os.getenv("JWT_SECRET")
+JWT_EXP_MINS= int(os.getenv("JWT_EXP_MINS"))
