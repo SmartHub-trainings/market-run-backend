bd166684cddc7dbf70ba78684d992a692b0ae460
georgebsmith-tech <georgebsmith.tech@gmail.com>
2026-09-25 15:25:33 +0100

    Moved some files

---
+++ routes/utils.py
@@ -0,0 +1,14 @@
+from schemas.auth_schema import GenerateOTP
+from datetime import datetime,timedelta
+from random import choices
+from secret import OTP_EXPIRATION
+
+
+def generate_otp()->GenerateOTP:
+    otp = "".join(choices("0123456789",k=6))
+    expires_at = datetime.now()+ timedelta(minutes=OTP_EXPIRATION)
+    return GenerateOTP(
+        otp,
+        expires_at
+    )
+
